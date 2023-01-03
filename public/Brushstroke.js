c2b59c6e071b6f171b40f80eee1a6379bf997044
sektionschef <the.stevee@gmail.com>
2023-01-03 09:35:48 +0100

    added comments and targetDyn

--- public/Brushstroke.js
+++ public/Brushstroke.js
@@ -1,27 +1,28 @@
+// grasp maxforce;
+
 class Brushstroke {
     constructor(sprite) {
-        this.maxSpeed = 15;
-        this.minSpeed = 2;
-        this.desiredSpeed = 0;
-        this.maxForce = 2;
-        this.slowRadius = 500;  // when to slow down
-        this.DEBUG = false;
+        this.maxSpeed = 15;  // top speed limit
+        this.minSpeed = 2;  // minimum speed - prevents from stopping at 0
+        this.maxForce = 2;  // agility for changes, if too little -> overshoot
+        this.slowRadius = 500;  // radius in which to slow down
 
-        this.basicSizeMin = 50;  // for debug
-        this.basicSizeMax = 100;  // for debug
-        this.basicSize = this.basicSizeMax;  // for debug
+        this.DEBUG = true;
+        // for dynamic resizing
+        this.basicSizeMin = 50;
+        this.basicSizeMax = 100;
+        this.basicSize = this.basicSizeMax;
 
+        this.desiredSpeed = 0;  // initial value, dynamic actual speed
         this.target = createVector(width / 4 * 3, height / 8 * 6);
         this.origin = createVector(width / 2, height / 2);
 
-        this.pos = this.origin.copy();
-        // this.vel = createVector(0, 0);
+        this.pos = this.origin.copy();  // start position
+        this.targetDyn = this.target.copy();
         this.vel = p5.Vector.sub(this.target, this.origin);  // make the starting angle
         this.acc = createVector(0, 0);
 
         this.sprite = sprite;
-
-
     }
 
     update() {
@@ -31,6 +32,11 @@ class Brushstroke {
         this.acc.set(0, 0);
 
         this.basicSize = Math.round(map(this.desiredSpeed, 0, this.maxSpeed, this.basicSizeMax, this.basicSizeMin));
+
+        // this.target = createVector(mouseX, mouseY);
+        if (this.distanceToTarget < 500) {
+            this.targetDyn = createVector(500, 500);
+        }
     }
 
     applyForce(force) {
@@ -38,10 +44,10 @@ class Brushstroke {
     }
 
     seek() {
-        let force = p5.Vector.sub(this.target, this.pos);
+        let force = p5.Vector.sub(this.targetDyn, this.pos);
         this.desiredSpeed = this.maxSpeed;
 
-        // launching
+        // launching at lower speed
         this.distanceToOrigin = p5.Vector.sub(this.origin, this.pos).mag();
         // console.log(this.distanceToOrigin);
         if (this.distanceToOrigin < this.slowRadius) {
@@ -49,7 +55,7 @@ class Brushstroke {
             // this.desiredSpeed = 50;
         }
 
-        // arrival at target
+        // arrival at target with lower speed
         this.distanceToTarget = force.mag();
         if (this.distanceToTarget < this.slowRadius) {
             this.desiredSpeed = map(this.distanceToTarget, 0, this.slowRadius, 0, this.maxSpeed);
@@ -85,7 +91,7 @@ class Brushstroke {
 
         push();
         translate(this.pos.x, this.pos.y);
-        fill(color(PALETTE.pixelColors[2]));
+        if (this.DEBUG) { fill(color("blue")) };
         noStroke();
         rotate(this.vel.heading())
         imageMode(CENTER);
@@ -102,7 +108,7 @@ class Brushstroke {
         if (this.DEBUG) {
             // target DEBUG        
             push();
-            translate(this.target.x, this.target.y);
+            translate(this.targetDyn.x, this.targetDyn.y);
             fill(color("green"));
             noStroke();
             circle(0, 0, 50);
@@ -110,7 +116,7 @@ class Brushstroke {
 
             // target slowRadius
             push();
-            translate(this.target.x, this.target.y);
+            translate(this.targetDyn.x, this.targetDyn.y);
             strokeWeight(5);
             stroke(color("green"));
             noFill();
