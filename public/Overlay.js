70022f9f1f26ddc436ba092d0fd7f9120c98db6a
sektionschef <the.stevee@gmail.com>
2023-01-12 21:53:26 +0100

    overlay working

--- public/Overlay.js
+++ public/Overlay.js
@@ -9,28 +9,11 @@ class Overlay {
 
         this.buffer.curveTightness(this.curveSexyness);
 
-        this.offsetMin = 0.99;
-        this.offsetMax = 1.01;
-        this.randomOffset1 = getRandomFromInterval(this.offsetMin, this.offsetMax);
-        this.randomOffset2 = getRandomFromInterval(this.offsetMin, this.offsetMax);
-        this.randomOffset3 = getRandomFromInterval(this.offsetMin, this.offsetMax);
-        this.randomOffset4 = getRandomFromInterval(this.offsetMin, this.offsetMax);
-        this.randomOffset5 = getRandomFromInterval(this.offsetMin, this.offsetMax);
-        this.randomOffset6 = getRandomFromInterval(this.offsetMin, this.offsetMax);
 
-        this.A = createVector(0 - width * 1.1, height / 8 * 3);
-        this.A1 = createVector(width / 9 * 2, height / 8 * 3 * this.randomOffset1);
-        this.A2 = createVector(width / 9 * 5, height / 8 * 3 * this.randomOffset2);
-        this.A3 = createVector(width / 9 * 7, height / 8 * 3 * this.randomOffset3);
-        this.B = createVector(width * 1.1, height / 8 * 3);
-        this.C = createVector(width * 1.1, height / 8 * 5);
-        this.C1 = createVector(width / 9 * 7, height / 8 * 5 * this.randomOffset4);
-        this.C2 = createVector(width / 9 * 5, height / 8 * 5 * this.randomOffset5);
-        this.C3 = createVector(width / 9 * 2, height / 8 * 5 * this.randomOffset6);
-        this.D = createVector(0 - width * 1.1, height / 8 * 5);
-
+        this.createCoverOverlay();
         this.coverOverlay();
 
+        // this.createBackgroundFoldCoords();
         // this.createBackgroundFold();
         // this.createUpperLine();
         // this.createLowerLine();
@@ -70,6 +53,28 @@ class Overlay {
             this.buffer.pop();
         }
 
+    }
+
+    createBackgroundFoldCoords() {
+        this.offsetMin = 0.99;
+        this.offsetMax = 1.01;
+        this.randomOffset1 = getRandomFromInterval(this.offsetMin, this.offsetMax);
+        this.randomOffset2 = getRandomFromInterval(this.offsetMin, this.offsetMax);
+        this.randomOffset3 = getRandomFromInterval(this.offsetMin, this.offsetMax);
+        this.randomOffset4 = getRandomFromInterval(this.offsetMin, this.offsetMax);
+        this.randomOffset5 = getRandomFromInterval(this.offsetMin, this.offsetMax);
+        this.randomOffset6 = getRandomFromInterval(this.offsetMin, this.offsetMax);
+
+        this.A = createVector(0 - width * 1.1, height / 8 * 3);
+        this.A1 = createVector(width / 9 * 2, height / 8 * 3 * this.randomOffset1);
+        this.A2 = createVector(width / 9 * 5, height / 8 * 3 * this.randomOffset2);
+        this.A3 = createVector(width / 9 * 7, height / 8 * 3 * this.randomOffset3);
+        this.B = createVector(width * 1.1, height / 8 * 3);
+        this.C = createVector(width * 1.1, height / 8 * 5);
+        this.C1 = createVector(width / 9 * 7, height / 8 * 5 * this.randomOffset4);
+        this.C2 = createVector(width / 9 * 5, height / 8 * 5 * this.randomOffset5);
+        this.C3 = createVector(width / 9 * 2, height / 8 * 5 * this.randomOffset6);
+        this.D = createVector(0 - width * 1.1, height / 8 * 5);
     }
 
     createBackgroundFold() {
@@ -130,62 +135,139 @@ class Overlay {
         this.buffer.pop();
     }
 
+    createBackgroundFoldCoords() {
+        this.offsetMin = 0.99;
+        this.offsetMax = 1.01;
+        this.randomOffset1 = getRandomFromInterval(this.offsetMin, this.offsetMax);
+        this.randomOffset2 = getRandomFromInterval(this.offsetMin, this.offsetMax);
+        this.randomOffset3 = getRandomFromInterval(this.offsetMin, this.offsetMax);
+        this.randomOffset4 = getRandomFromInterval(this.offsetMin, this.offsetMax);
+        this.randomOffset5 = getRandomFromInterval(this.offsetMin, this.offsetMax);
+        this.randomOffset6 = getRandomFromInterval(this.offsetMin, this.offsetMax);
+
+        this.A = createVector(0 - width * 1.1, height / 8 * 3);
+        this.A1 = createVector(width / 9 * 2, height / 8 * 3 * this.randomOffset1);
+        this.A2 = createVector(width / 9 * 5, height / 8 * 3 * this.randomOffset2);
+        this.A3 = createVector(width / 9 * 7, height / 8 * 3 * this.randomOffset3);
+        this.B = createVector(width * 1.1, height / 8 * 3);
+        this.C = createVector(width * 1.1, height / 8 * 5);
+        this.C1 = createVector(width / 9 * 7, height / 8 * 5 * this.randomOffset4);
+        this.C2 = createVector(width / 9 * 5, height / 8 * 5 * this.randomOffset5);
+        this.C3 = createVector(width / 9 * 2, height / 8 * 5 * this.randomOffset6);
+        this.D = createVector(0 - width * 1.1, height / 8 * 5);
+    }
+
+    createCoverTop() {
+        this.TopA = createVector(0 - this.offsetCanvas, 0 - this.offsetCanvas);
+        this.TopB = createVector(width + this.offsetCanvas, 0 - this.offsetCanvas);
+        this.TopC = createVector(width + this.offsetCanvas, height / 8 * 1);
+        this.TopD = createVector(width / 8 * 5, height / 8 * 1 + this.randomOffset1);
+        this.TopE = createVector(width / 8 * 4, height / 8 * 1 + this.randomOffset2);
+        this.TopF = createVector(width / 8 * 3, height / 8 * 1 + this.randomOffset3);
+        this.TopG = createVector(0 - this.offsetCanvas, height / 8 * 1);
+    }
+
+    createCoverLeft() {
+        this.LeftA = createVector(0 - this.offsetCanvas, 0 - this.offsetCanvas);
+        this.LeftB = createVector(width / 8 * 3, 0 - this.offsetCanvas);
+        this.LeftC = createVector(width / 8 * 3 + this.randomOffset1, height / 8 * 1);
+        this.LeftD = createVector(width / 8 * 3 + this.randomOffset2, height / 8 * 4);
+        this.LeftE = createVector(width / 8 * 3 + this.randomOffset3, height / 8 * 7);
+        this.LeftF = createVector(width / 8 * 3, height + this.offsetCanvas);
+        this.LeftG = createVector(0 - this.offsetCanvas, height + this.offsetCanvas);
+    }
+
+    createCoverBottom() {
+        this.BottomA = createVector(0 - this.offsetCanvas, height / 8 * 7);
+        this.BottomB = createVector(width / 8 * 3, height / 8 * 7 + this.randomOffset1);
+        this.BottomC = createVector(width / 8 * 4, height / 8 * 7 + this.randomOffset2);
+        this.BottomD = createVector(width / 8 * 5, height / 8 * 7 + this.randomOffset3);
+        this.BottomE = createVector(width + this.offsetCanvas, height / 8 * 7);
+        this.BottomF = createVector(width + this.offsetCanvas, height + this.offsetCanvas);
+        this.BottomG = createVector(0 - this.offsetCanvas, height + this.offsetCanvas);
+    }
+
+    createCoverRight() {
+        this.RightA = createVector(width / 8 * 5, 0 - this.offsetCanvas);
+        this.RightB = createVector(width / 8 * 5 + this.randomOffset1, height / 8 * 1);
+        this.RightC = createVector(width / 8 * 5 + this.randomOffset1, height / 8 * 4);
+        this.RightD = createVector(width / 8 * 5 + this.randomOffset1, height / 8 * 7);
+        this.RightE = createVector(width / 8 * 5, height + this.offsetCanvas);
+        this.RightF = createVector(width + this.offsetCanvas, height + this.offsetCanvas);
+        this.RightG = createVector(width + this.offsetCanvas, 0 - this.offsetCanvas);
+    }
+
+    createCoverOverlay() {
+        this.offset = 0.01;
+        this.randomOffset1 = DOMINANTSIDE * getRandomFromInterval(-this.offset, this.offset);
+        this.randomOffset2 = DOMINANTSIDE * getRandomFromInterval(-this.offset, this.offset);
+        this.randomOffset3 = DOMINANTSIDE * getRandomFromInterval(-this.offset, this.offset);
+
+        this.offsetCanvas = DOMINANTSIDE * 0.1;
+
+        this.createCoverTop();
+        this.createCoverLeft();
+        this.createCoverBottom();
+        this.createCoverRight();
+    }
+
     coverOverlay() {
         // OVERLAY TO DINGS
         this.buffer.noStroke();
-        // this.buffer.stroke(50);
         this.buffer.fill(this.overlayColor);
+
+        this.createCoverOverlay();
 
         // top
         this.buffer.beginShape();
-        this.buffer.curveVertex(0, 0);
-        this.buffer.curveVertex(0, 0);
-        this.buffer.curveVertex(0, height / 8 * 3);
-        this.buffer.curveVertex(width / 8 * 3, height / 8 * 1);
-        this.buffer.curveVertex(width / 8 * 4, height / 8 * 1);
-        this.buffer.curveVertex(width / 8 * 5, height / 8 * 1);
-        this.buffer.curveVertex(width, height / 8 * 3);
-        this.buffer.curveVertex(width, 0);
-        this.buffer.curveVertex(width, 0);
+        this.buffer.curveVertex(this.TopA.x, this.TopA.y);
+        this.buffer.curveVertex(this.TopA.x, this.TopA.y);
+        this.buffer.curveVertex(this.TopB.x, this.TopB.y);
+        this.buffer.curveVertex(this.TopC.x, this.TopC.y);
+        this.buffer.curveVertex(this.TopD.x, this.TopD.y);
+        this.buffer.curveVertex(this.TopE.x, this.TopE.y);
+        this.buffer.curveVertex(this.TopF.x, this.TopF.y);
+        this.buffer.curveVertex(this.TopG.x, this.TopG.y);
+        this.buffer.curveVertex(this.TopG.x, this.TopG.y);
         this.buffer.endShape();
 
-        // left
+        // // left
         this.buffer.beginShape();
-        this.buffer.curveVertex(0, 0);
-        this.buffer.curveVertex(0, 0);
-        this.buffer.curveVertex(width / 8 * 4, 0);
-        this.buffer.curveVertex(width / 8 * 3, height / 8 * 1);
-        this.buffer.curveVertex(width / 8 * 3, height / 8 * 4);
-        this.buffer.curveVertex(width / 8 * 3, height / 8 * 7);
-        this.buffer.curveVertex(width / 8 * 4, height);
-        this.buffer.curveVertex(0, height);
-        this.buffer.curveVertex(0, height);
+        this.buffer.curveVertex(this.LeftA.x, this.LeftA.y);
+        this.buffer.curveVertex(this.LeftA.x, this.LeftA.y);
+        this.buffer.curveVertex(this.LeftB.x, this.LeftB.y);
+        this.buffer.curveVertex(this.LeftC.x, this.LeftC.y);
+        this.buffer.curveVertex(this.LeftD.x, this.LeftD.y);
+        this.buffer.curveVertex(this.LeftE.x, this.LeftE.y);
+        this.buffer.curveVertex(this.LeftF.x, this.LeftF.y);
+        this.buffer.curveVertex(this.LeftG.x, this.LeftG.y);
+        this.buffer.curveVertex(this.LeftG.x, this.LeftG.y);
         this.buffer.endShape();
 
-        // bottom
+        // // bottom
         this.buffer.beginShape();
-        this.buffer.curveVertex(0, height);
-        this.buffer.curveVertex(0, height);
-        this.buffer.curveVertex(0, height / 8 * 6);
-        this.buffer.curveVertex(width / 8 * 3, height / 8 * 7);
-        this.buffer.curveVertex(width / 8 * 4, height / 8 * 7);
-        this.buffer.curveVertex(width / 8 * 5, height / 8 * 7);
-        this.buffer.curveVertex(width, height / 8 * 6);
-        this.buffer.curveVertex(width, height);
-        this.buffer.curveVertex(width, height);
+        this.buffer.curveVertex(this.BottomA.x, this.BottomA.y);
+        this.buffer.curveVertex(this.BottomA.x, this.BottomA.y);
+        this.buffer.curveVertex(this.BottomB.x, this.BottomB.y);
+        this.buffer.curveVertex(this.BottomC.x, this.BottomC.y);
+        this.buffer.curveVertex(this.BottomD.x, this.BottomD.y);
+        this.buffer.curveVertex(this.BottomE.x, this.BottomE.y);
+        this.buffer.curveVertex(this.BottomF.x, this.BottomF.y);
+        this.buffer.curveVertex(this.BottomG.x, this.BottomG.y);
+        this.buffer.curveVertex(this.BottomG.x, this.BottomG.y);
         this.buffer.endShape();
 
-        // right
+        // // right
         this.buffer.beginShape();
-        this.buffer.curveVertex(width, 0);
-        this.buffer.curveVertex(width, 0);
-        this.buffer.curveVertex(width / 8 * 4, 0);
-        this.buffer.curveVertex(width / 8 * 5, height / 8 * 1);
-        this.buffer.curveVertex(width / 8 * 5, height / 8 * 4);
-        this.buffer.curveVertex(width / 8 * 5, height / 8 * 7);
-        this.buffer.curveVertex(width / 8 * 4, height);
-        this.buffer.curveVertex(width, height);
-        this.buffer.curveVertex(width, height);
+        this.buffer.curveVertex(this.RightA.x, this.RightA.y);
+        this.buffer.curveVertex(this.RightA.x, this.RightA.y);
+        this.buffer.curveVertex(this.RightB.x, this.RightB.y);
+        this.buffer.curveVertex(this.RightC.x, this.RightC.y);
+        this.buffer.curveVertex(this.RightD.x, this.RightD.y);
+        this.buffer.curveVertex(this.RightE.x, this.RightE.y);
+        this.buffer.curveVertex(this.RightF.x, this.RightF.y);
+        this.buffer.curveVertex(this.RightG.x, this.RightG.y);
+        this.buffer.curveVertex(this.RightG.x, this.RightG.y);
         this.buffer.endShape();
     }
 
