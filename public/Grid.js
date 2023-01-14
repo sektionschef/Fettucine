9a909e08638c2f13f1ef4e4fbad9795812cdb9c0
sektionschef <the.stevee@gmail.com>
2023-01-14 09:52:31 +0100

    first four

--- public/Grid.js
+++ public/Grid.js
@@ -1,10 +1,11 @@
 class Grid {
     constructor() {
-        this.margin = DOMINANTSIDE * 0.1;
-        this.boxCount = 9;
+        // this.margin = 0; // DOMINANTSIDE * 0.1;
+        this.profile = "1/3-1/6";
 
-        this.opWidth = width - this.margin * 2;
-        this.opHeight = height - this.margin * 2;
+
+        this.opWidth = width //- this.margin * 2;
+        this.opHeight = height //- this.margin * 2;
 
         if (this.opWidth < this.opHeight) {
             this.subSide = this.opWidth;
@@ -14,10 +15,20 @@ class Grid {
             this.domSide = this.opWidth;
         }
 
+        // only the rest
+        // TODO center for this.margin/2;
+        // console.log(this.subSide % 50 == 0);
+        this.margin = this.subSide % 50;
+
+        this.boxCount = this.subSide / 50;
+        // console.log(this.boxCount);
+        // this.boxCount = 9;
+
         this.boxSize = this.subSide / this.boxCount;
         this.boxCountDom = this.domSide / this.boxSize;
 
         this.boxes = [];
+        var index = 0;
 
         for (var d = 0; d < (this.boxCountDom); d++) {
             for (var s = 0; s < (this.boxCount); s++) {
@@ -40,8 +51,11 @@ class Grid {
                     "C": C,
                     "D": D,
                     "dominant": d,
-                    "sub": s
+                    "sub": s,
+                    "index": index,
                 })
+
+                index += 1;
             }
         }
     }
@@ -57,35 +71,50 @@ class Grid {
 
 
         // view cols and rows
-        // for (var i = 0; i < (this.boxCount + 1); i++) {
-        //     strokeWeight(10);
-        //     line(this.margin + i * this.boxSize, 0, this.margin + i * this.boxSize, height);
-        // }
+        for (var i = 0; i < (this.boxCount + 1); i++) {
+            strokeWeight(10);
+            line(this.margin + i * this.boxSize, 0, this.margin + i * this.boxSize, height);
+        }
 
-        // for (var i = 0; i < (this.boxCountDom + 1); i++) {
-        //     strokeWeight(10);
-        //     line(0, this.margin + i * this.boxSize, width, this.margin + i * this.boxSize);
-        // }
+        for (var i = 0; i < (this.boxCountDom + 1); i++) {
+            strokeWeight(10);
+            line(0, this.margin + i * this.boxSize, width, this.margin + i * this.boxSize);
+        }
     }
     show() {
 
         // this.showDebug();
 
-        for (var box of this.boxes) {
-            if (box.dominant == 4) {
-                noStroke();
-                // strokeWeight(3);
-                fill("white");
-                rect(box.A.x, box.A.y, this.boxSize, this.boxSize);
-            }
+        this.third = Math.round(this.boxCount / 3);
+        this.stripe = Math.round(this.third / 8);
 
-            if (box.sub == 1) {
-                noStroke();
-                // strokeWeight(3);
-                fill("white");
-                rect(box.A.x, box.A.y, this.boxSize, this.boxSize);
+        for (var box of this.boxes) {
+            // if (box.dominant == 4) {
+            //     noStroke();
+            //     // strokeWeight(3);
+            //     fill("white");
+            //     rect(box.A.x, box.A.y, this.boxSize, this.boxSize);
+            // }
+
+            // if (box.sub == 1) {
+            //     noStroke();
+            //     // strokeWeight(3);
+            //     fill("white");
+            //     rect(box.A.x, box.A.y, this.boxSize, this.boxSize);
+            // }
+
+            // if (box.sub >= this.third && box.sub < this.third * 2) {
+            for (var i = 0; i < 8; i++) {
+                if (box.sub >= this.stripe * i && box.sub < this.stripe * (i + 1)) {
+                    if (i % 2 == 0) {
+                        noStroke();
+                        // strokeWeight(3);
+                        fill("white");
+                        rect(box.A.x, box.A.y, this.boxSize, this.boxSize);
+                    }
+                }
+
             }
         }
-
     }
 }
