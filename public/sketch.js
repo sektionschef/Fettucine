b530d03c72923af0766bb48c1605bd87bcabbf23
sektionschef <the.stevee@gmail.com>
2023-02-17 17:33:40 +0100

    palette in own file

--- public/sketch.js
+++ public/sketch.js
@@ -7,8 +7,6 @@ let rescaling_height;
 let backgroundTexture;
 
 let ANIMATIONSTATE = true;
-let PALETTE;
-let PALETTE_LABEL;
 let ALLDONE = false;
 
 let TITLE = "Materialista";
@@ -20,65 +18,6 @@ let PRICE = "ꜩ 4";
 let EDITIONS = "256 editions";
 
 let CURRENTPIXELDENS = 1;
-
-const PALETTESYSTEM = {
-  "Papa": {
-    "cardboard": "#d80f0fff",
-    "paper": "#fafafa",
-    "darkColor": "#c70303ff",
-    "lightColor": "#f51c1cff",
-  },
-  "Suburban": {
-    "cardboard": "#d80f0fff",
-    "paper": "#fafafa",
-    "darkColor": "#af0606ff",
-    "lightColor": "#f02626ff",
-  },
-  "Wake Board": {
-    "cardboard": "#d80f0fff",
-    "paper": "#fafafa",
-    "darkColor": "#860202ff",
-    "lightColor": "#f53c3cff",
-  },
-  "Emmerald": {
-    "cardboard": "#edb0ffff",
-    "paper": "#fafafa",
-    "darkColor": "#f575b5ff",
-    "lightColor": "#5858e4ff",
-  },
-  "New Red Baron": {
-    "cardboard": "#ff0000ff",
-    "paper": "#fafafa",
-    "darkColor": "#860202ff",
-    "lightColor": "#f53c3cff",
-  },
-  "Yellow Mowy": {
-    "cardboard": "#ffba39ff",
-    "paper": "#fafafa",
-    "darkColor": "#f5885cff",
-    "lightColor": "#fcb073ff",
-  },
-  "Hasenhüttl": {
-    "cardboard": "#ffa600ff",
-    "paper": "#fafafa",
-    "darkColor": "#a76c00ff",
-    "lightColor": "#fdd281ff",
-  },
-  "Grind": {
-    "cardboard": "#314ce4ff",
-    "paper": "#fafafa",
-    "darkColor": "#0d1a63ff",
-    "lightColor": "#3047ceff",
-  },
-  "Alles gut mein Kind": {
-    "cardboard": "#273797ff",
-    "paper": "#fafafa",
-    "darkColor": "#0d1a63ff",
-    "lightColor": "#3047ceff",
-  },
-}
-
-choosePalette();
 
 
 function preload() {
